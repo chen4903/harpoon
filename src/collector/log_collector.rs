1eea73ebac1e6bafcb8c603c8e02d93b308dbd1e
chenqin <490358423@qq.com>
2025-10-21 22:22:02 +0800

    feat: log collector

---
+++ src/collector/log_collector.rs
@@ -0,0 +1,34 @@
+use std::sync::Arc;
+
+use alloy::{
+    providers::Provider,
+    rpc::types::eth::{Filter, Log},
+};
+use async_trait::async_trait;
+use futures::StreamExt;
+
+use crate::interface::{CollectorInterface, collector::CollectorStream};
+
+pub struct LogCollector {
+    provider: Arc<dyn Provider>,
+    filter: Filter,
+}
+
+impl LogCollector {
+    pub fn new(provider: Arc<dyn Provider>, filter: Filter) -> Self {
+        Self { provider, filter }
+    }
+}
+
+#[async_trait]
+impl CollectorInterface<Log> for LogCollector {
+    fn name(&self) -> &str {
+        "Log Collector"
+    }
+
+    async fn get_event_stream(&self) -> eyre::Result<CollectorStream<'_, Log>> {
+        let stream = self.provider.subscribe_logs(&self.filter).await?;
+        let stream = stream.into_stream().filter_map(|v| async move { Some(v) });
+        Ok(Box::pin(stream))
+    }
+}
--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -1,3 +1,4 @@
 pub mod block_collector;
 pub mod full_block_collector;
 pub mod interval_collector;
+pub mod log_collector;
