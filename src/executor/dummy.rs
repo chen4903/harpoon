ea3d4788f37d0ff71e8dc304a0341a02bcf2a149
chenqin <490358423@qq.com>
2025-10-21 22:06:06 +0800

    chore: add dummy

---
+++ src/executor/dummy.rs
@@ -0,0 +1,34 @@
+use crate::interface::ExecutorInterface;
+use async_trait::async_trait;
+
+/// This executor is primarily used for testing purposes to verify if collectors and strategies
+/// are working properly without executing real operations.
+///
+/// # Examples
+///
+/// ```
+/// use crate::{Engine, executor::Dummy};
+///
+/// let engine = Engine::new()
+///     .add_collector(some_collector)
+///     .add_strategy(some_strategy)
+///     .add_executor(Dummy);
+/// ```
+///
+/// # Note
+///
+/// This executor will always return `Ok(())` regardless of the input action.
+pub struct Dummy;
+
+#[async_trait]
+impl<A: Send + Sync + 'static> ExecutorInterface<A> for Dummy {
+    fn name(&self) -> &str {
+        "Dummy"
+    }
+
+    async fn execute(&self, action: A) -> eyre::Result<()> {
+        let _ = action;
+
+        Ok(())
+    }
+}
