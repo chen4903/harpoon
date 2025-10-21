c0061b5b6e43b59b6f2b5c8cdb8e29ed23c3e1b4
chenqin <490358423@qq.com>
2025-10-21 22:25:43 +0800

    feat: logs in block collector

---
+++ src/collector/logs_in_block_collector.rs
@@ -0,0 +1,64 @@
+use std::sync::Arc;
+
+use alloy::{
+    primitives::BlockHash,
+    providers::Provider,
+    rpc::types::{
+        Header,
+        eth::{Filter, Log},
+    },
+};
+use async_trait::async_trait;
+use futures::StreamExt;
+
+use crate::interface::{CollectorInterface, collector::CollectorStream};
+
+pub struct LogsInBlockCollector {
+    provider: Arc<dyn Provider>,
+    filter: Filter,
+}
+
+impl LogsInBlockCollector {
+    pub fn new(provider: Arc<dyn Provider>, filter: Filter) -> Self {
+        Self { provider, filter }
+    }
+
+    async fn block_to_logs(&self, block_hash: BlockHash) -> Option<Vec<Log>> {
+        let logs = self
+            .provider
+            .get_logs(&self.filter.clone().at_block_hash(block_hash))
+            .await;
+
+        match logs {
+            Ok(logs) => Some(logs),
+            Err(e) => {
+                tracing::error!(?block_hash, "fail to get logs: {e:#}");
+                None
+            }
+        }
+    }
+}
+
+#[async_trait]
+impl CollectorInterface<(Header, Vec<Log>)> for LogsInBlockCollector {
+    fn name(&self) -> &str {
+        "Logs In Block Collector"
+    }
+
+    async fn get_event_stream(&self) -> eyre::Result<CollectorStream<'_, (Header, Vec<Log>)>> {
+        let mut stream = self.provider.subscribe_blocks().await?.into_stream();
+
+        let stream = async_stream::stream! {
+            while let Some(block) = stream.next().await {
+                let logs = match self.block_to_logs(block.hash).await {
+                    Some(logs) => logs,
+                    None => continue,
+                };
+
+                yield (block, logs);
+            }
+        };
+
+        Ok(Box::pin(stream))
+    }
+}
--- src/collector/mod.rs
+++ src/collector/mod.rs
@@ -2,3 +2,4 @@ pub mod block_collector;
 pub mod full_block_collector;
 pub mod interval_collector;
 pub mod log_collector;
+pub mod logs_in_block_collector;
